80ce15cec47fa83c22346842de2ce0ab62fb527f
Anber Jamshed <anberjam@yahoo.com>
2020-09-23 01:51:00 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,8 +37,10 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for]
 
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,8 +58,10 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages][0]  
 
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -81,7 +86,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash [:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+  
+programmer_hash
 end
 
 def changing_alan
@@ -103,6 +113,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]= "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +137,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
+    
+    programmer_hash
 
 end
