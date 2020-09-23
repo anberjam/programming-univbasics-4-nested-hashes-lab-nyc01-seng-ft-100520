207039161a773816ecddf24cbd97d5bdb7fd4619
Anber Jamshed <anberjam@yahoo.com>
2020-09-23 01:16:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,10 +73,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		  :yukihiro_matsumoto => {
- 		    :known_for => "Ruby",
- 		    :languages => ["LISP", "C"]
- 		  },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -90,6 +86,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto] = {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP", "C"]
+ 		  }
 
 end
 
