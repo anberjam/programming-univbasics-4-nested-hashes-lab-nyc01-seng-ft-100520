c73fb402710d8dbc114665834c28df2dcc2436c4
Anber Jamshed <anberjam@yahoo.com>
2020-09-23 01:47:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,8 +135,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages].shift("Assembly")
+    programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
     
-    programmer_hash
+    return programmer_hash
 
 end
